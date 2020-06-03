6f6380fb3787501d2f885ae133344c20e416340a
afeiship <1290657123@qq.com>
2020-06-03 16:48:11 +0800

    refactor: use tmpl for template

--- __tests__/api.spec.js
+++ __tests__/api.spec.js
@@ -2,11 +2,10 @@ const nx = require('@feizheng/next-js-core2');
 require('../src/next-git-log');
 
 describe('api.basic test', () => {
-  test('nx.gitLog', function() {
-    const obj1 = { name: 'fei' };
-    const obj2 = { email: '1290657123@qq.com' };
-    const result = {};
-    nx.gitLog(result, obj1, obj2);
-    expect(result.name, obj1.name).toBe(null);
+  test('nx.gitLog', function (done) {
+    nx.gitLog({ before: '2020-06-03' }).then(res => {
+      console.log(res);
+      done();
+    });
   });
 });
--- src/next-git-log.js
+++ src/next-git-log.js
@@ -1,10 +1,27 @@
 (function () {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('@feizheng/next-js-core2');
-  var execSync = require('child_process').execSync;
+  var tmpl = nx.tmpl || require('@feizheng/next-tmpl');
+  var exec = require('child_process').exec;
+  var DEFAULT_OPTIONS = {
+    pretty: "%s",
+    since: '2020-05-01',
+    before: '2020-06-01',
+  }
 
-  nx.gitLog = function () {
-    //code goes here.
+  nx.gitLog = function (inOptions) {
+    var options = nx.mix(null, DEFAULT_OPTIONS, inOptions);
+    var cmds = tmpl('git log --pretty="{pretty}" --since="{since}" --before="{before}"', options);
+
+    return new Promise(function (resolve, reject) {
+      exec(cmds, function (error, stdout) {
+        if (error) {
+          reject(error);
+          return;
+        }
+        resolve(stdout);
+      });
+    })
   };
 
   if (typeof module !== 'undefined' && module.exports) {
